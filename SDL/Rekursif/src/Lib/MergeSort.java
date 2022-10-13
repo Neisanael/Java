045d10bb0861f02a955d53368f8d6e5dcfc381b3
Neisanael <91104239+Neisanael@users.noreply.github.com>
2022-10-13 08:57:10 +0700

    Update MergeSort.java

--- SDL/Rekursif/src/Lib/MergeSort.java
+++ SDL/Rekursif/src/Lib/MergeSort.java
@@ -9,49 +9,46 @@ import java.util.Arrays;
 
 public class MergeSort {
 
-    private static int passCount;
-
-    public static void mergeSort(int[] list) {
-        passCount = 0;
-        mergeSort(list, 0, list.length);
-    }
-
-    private static void mergeSort(int[] list, int front, int back) //O( Log N )
-    {
-        int mid = (front + back) / 2;
-        if (mid == front) {
-            return;
+public static void main(String[] args) {
+        int[] arr = {5, 8, 26, 15, 11, 31, 7, 40, 23, 25};
+        int[] dest = {5, 8, 26, 15, 11, 31, 7, 40, 23, 25};
+        
+        mergeSort(arr, dest, 0, arr.length);
+        for (int i = 0; i < dest.length; i++) {
+            System.out.println(dest[i]);
         }
-        mergeSort(list, front, mid);
-        mergeSort(list, mid, back);
-        merge(list, front, back);
-
     }
-
-    private static void merge(int[] list, int front, int back) //O(N)
-    {
-        int dif = back - front;
-        int[] temp = new int[dif];
-        int beg = front, mid = (front + back) / 2;
-        int saveMid = mid;
-        int spot = 0;
-
-        while (beg < saveMid && mid < back) {
-            if (list[beg] < list[mid]) {
-                temp[spot++] = list[beg++];
-            } else {
-                temp[spot++] = list[mid++];
+    
+    public static void mergeSort(int[] arr, int[] dest,int awal, int akhir){
+        int length = akhir - awal;
+        if(length < 2){
+            return;
+        }else {
+            int mid = (awal + akhir)/2;
+            mergeSort(dest, arr, awal, mid);
+            mergeSort(dest, arr, mid, akhir);
+            if(arr[mid -1]<= arr[mid]){
+                System.arraycopy(arr, 0, dest, 0, arr.length);
+            }
+            int p = awal, q = mid, i = awal;
+            while (i< akhir){
+                if(arr[p]<=arr[q]){
+                    dest[i++] = arr[p++];
+                    if(p == mid){
+                        while(i< akhir){
+                            dest[i++] = arr[q++];
+                        }
+                    }
+                }else{
+                    dest[i++] = arr[q++];
+                    if(q==akhir){
+                        while(i<akhir){
+                            dest[i++] = arr[p++];
+                        }
+                    }
+                }
             }
         }
-        while (beg < saveMid) {
-            temp[spot++] = list[beg++];
-        }
-        while (mid < back) {
-            temp[spot++] = list[mid++];
-        }
-        for (int i = 0; i < back - front; i++) {
-            list[front + i] = temp[i];
-        }
-        System.out.println("pass " + passCount++ + " " + Arrays.toString(list) + "\n");
     }
 }
+
