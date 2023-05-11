a3c45d53ec30761649407433f4fd6ef22f78b3c0
Neisanael <91104239+Neisanael@users.noreply.github.com>
2023-05-11 21:03:51 +0700

    update graph

--- SNDL/Graph/src/graph/GraphMain.java
+++ SNDL/Graph/src/graph/GraphMain.java
@@ -1,26 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package graph;
 
-/**
- *
- * @author natha
- */
 public class GraphMain {
 
     public static void main(String[] args) {
         Graph graph = new Graph();
 
-        // add vertices
         graph.addVertex('A');
         graph.addVertex('B');
         graph.addVertex('C');
         graph.addVertex('E');
         graph.addVertex('F');
 
-        // add edges
         graph.addEdge('A', 'B', 4);
         graph.addEdge('B', 'C', 5);
         graph.addEdge('B', 'E', 4);
@@ -29,12 +19,9 @@ public class GraphMain {
         graph.addEdge('F', 'C', 3);
         graph.addEdge('B', 'F', 2);
         graph.addEdge('C', 'E', 5);
-        
 
-        // display the graph using the show() method
         graph.show();
 
-        // display the graph using the toString() method
         System.out.println(graph.toString());
     }
 }
